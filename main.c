9753f2697f43f3f1a214edb3ff0287bce9856670
Marian Sauer <marian.sauer@localhost>
2022-03-10 20:04:12 +0100

    Ping operational.

--- main.c
+++ main.c
@@ -6,29 +6,43 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <fcntl.h>
 
 void
-sio_send(u8_t c, sio_fd_t context)
+sio_send(u8_t c, sio_fd_t fd)
 {
-
+  int ret = write(fd,
+                  &c,
+                  sizeof(c));
 }
 
 sio_fd_t
 sio_open(u8_t devnum)
 {
-  return 1;
+  int fd = open("/dev/tnt0",
+                O_NONBLOCK | O_RDWR);
+
+  return (fd > 0) ? fd : 0;
 }
 
 uint32_t
 sio_tryread(sio_fd_t fd, uint8_t *data, uint32_t len)
 {
-  *data = ' ';
-  return 1;
+  int ret = read(fd,
+                 data,
+                 len);
+
+  return (ret > 0) ? ret : 0;
 }
 
 int
 main(int argc, char **argv)
 {
+// goal: HDLC in Normal response mode (because RS485 is shared by secondaries)
+// a) primary requests -> one of n secondary replies
+// or
+// b) primary request -> timeout (100 ms)
+
   uint8_t num_slip1 = 0;
   ip4_addr_t ipaddr_slip1;
   ip4_addr_t netmask_slip1;
@@ -38,19 +52,19 @@ main(int argc, char **argv)
   lwip_init();
 
   IP4_ADDR(&ipaddr_slip1,
-           172,
-           30,
-           115,
-           84);
+           10,
+           0,
+           0,
+           2);
   IP4_ADDR(&netmask_slip1,
            255,
            255,
            255,
            0);
   IP4_ADDR(&gw_slip1,
-           172,
-           30,
-           115,
+           10,
+           0,
+           0,
            1);
 
   struct netif *ret = netif_add(&slipif1,
